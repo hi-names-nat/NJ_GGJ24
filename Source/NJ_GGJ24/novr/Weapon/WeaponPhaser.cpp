faeb07411a319758f7c319b3a16bfa372f5ccac5
Julie Muzina <jmuzina@jmuzina.io>
2024-01-27 19:06:41 -0500

    phaser start boilerplate

---
+++ Source/NJ_GGJ24/novr/Weapon/WeaponPhaser.cpp
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "WeaponPhaser.h"
+
+#include "Camera/CameraComponent.h"
+#include "NJ_GGJ24/novr/Player/AsPlayer.h"
+#include "NJ_GGJ24/System/Character/HealthComponent.h"
+
+
+// Sets default values
+AWeaponPhaser::AWeaponPhaser()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AWeaponPhaser::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+bool AWeaponPhaser::Fire()
+{
+	if (!Super::Fire()) return false;
+
+	const APlayerController* PlayerController = Cast<APlayerController>(Player->GetController());
+	const FRotator SpawnRotation = PlayerController->PlayerCameraManager->GetCameraRotation();
+	// MuzzleOffset is in camera space, so transform it to world space before offsetting from the character location to find the final muzzle position
+	const FVector SpawnLocation = GetOwner()->GetActorLocation() + SpawnRotation.RotateVector(MuzzleOffset);
+	
+	FHitResult Out_Hit;
+	const FVector TargetLocation = SpawnLocation + (Player->GetCamera()->GetForwardVector() * MaxRange);
+
+	auto world = GetWorld();
+	// Trace using the hitscan channel
+	if (world->LineTraceSingleByChannel(Out_Hit, SpawnLocation, TargetLocation, ECC_GameTraceChannel2))
+	{
+		DrawDebugSphere(world, Out_Hit.Location, 15, 8, FColor::Red, false, 10);
+		DrawDebugLine(world, SpawnLocation, TargetLocation, FColor::Red, false, 10);
+		if (auto healthComponent = Cast<UHealthComponent>(Out_Hit.Component))
+		{
+			healthComponent->TakeDamage(RayDamage);
+		}
+		else
+		{
+			if (Out_Hit.Component->IsSimulatingPhysics())
+			{
+				Out_Hit.Component->AddImpulseAtLocation(TargetLocation-SpawnLocation * 25.F, Out_Hit.Location);
+			}
+		}
+	}
+	return true;
+}
+
+// Called every frame
+void AWeaponPhaser::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/NJ_GGJ24/novr/Weapon/WeaponPhaser.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WeaponActor.h"
+#include "WeaponPhaser.generated.h"
+
+UCLASS()
+class NJ_GGJ24_API AWeaponPhaser : public AWeaponActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AWeaponPhaser();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual bool Fire() override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Ammo")
+	float RayDamage;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+};
